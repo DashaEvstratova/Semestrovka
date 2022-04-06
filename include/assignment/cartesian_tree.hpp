984d75d8bfb03f83d39accf2393fa8092f87741e
DashaEvstratova <alis131140@gmail.com>
2022-04-06 23:25:42 +0300

    second

--- include/assignment/cartesian_tree.hpp
+++ include/assignment/cartesian_tree.hpp
@@ -10,7 +10,13 @@ namespace assignment {
 
         void Split(Node* node, int key, Node*& left, Node*& right);
 
+        Node *Merge(Node* left_tree, Node* right_tree) const;
+
+        void Insert(int key, int priority);
+
         bool Contains(int key) const;
+
+        bool Remove(int, key);
 
         bool IsEmpty() const;
 
--- src/cartesian_tree.cpp
+++ src/cartesian_tree.cpp
@@ -13,16 +13,59 @@ namespace assignment{
         clear(root_);
         root_ = nullptr;
     }
-    void CartesianTree::Split(Node* node, int key, Node*& left, Node*& right){
-        if (node == nullptr){
-
+    void CartesianTree::Split(Node* node, int key, Node*& left_tree, Node*& right_tree){
+        if (!node){
+            left_tree = nullptr;
+            right_tree = nullptr;
+            return
+        }
+        else if (node->key <= key){
+            Split(node->right, key, node->right, right_tree);
+            left_tree = node;
+        }
+        else {
+            Split(node->left, key, node->left, left_tree);
+            right_tree = node;
         }
     }
 
+
+    Node* Merge(Node* left_tree, Node* right_tree) {
+        if (left_tree == nullptr || right_tree == nullptr) {
+            return left_tree == 0 ? right_tree : left_tree;
+        }
+        if (left->priority > right->priority) {
+            left_tree->right = Merge(left_tree->right, right_tree);
+            return left_tree;
+        }
+        right_tree->left = Merge(left_tree, right_tree->left);
+        return right_tree;
+    }
+
+    void CartesianTree:: Insert(int key, int priority){
+        Node* left_tree, right_tree;
+        Split(root_, key, left_tree, right_tree);
+        left_tree = Merge(left_tree, new Node(key, priority));
+        root_ = Merge(left_tree, right_tree);
+    }
+
     bool CartesianTree::Contains(int key) const {
-        if (Find(key) == std::nullopt){
+        Node* left_tree, equal, right_tree;
+        Split(root_, key, left_tree, right_tree);
+        Split(right_tree, key+1, equal, right_tree)
+        bool result = equel;
+        root_ = Merge(Merge(left_tree, equal), right_tree);
+        return equel;
+    }
+
+    bool Remove(int, key){
+        if (!root_){
             return false;
         }
+        Node* left_tree, equal, right_tree;
+        Split(root_, key, left_tree, right_tree);
+        Split(right_tree, key+1, equal, right_tree);
+        root_ = Merge(left_tree, right_tree);
         return true;
     }
 
